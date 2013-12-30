1652faed5d6804aaf1e3b0536ce66dc1ab875676
viridian1138 <viridian1138@earthlink.net>
2013-12-29 17:20:05 -0700

    Make layout relative to support more tablets

--- VectorVictor/VectorVictor/src/com/postgreen/vectorvictor/VectorVictorActivity.java
+++ VectorVictor/VectorVictor/src/com/postgreen/vectorvictor/VectorVictorActivity.java
@@ -139,6 +139,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
